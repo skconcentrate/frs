4eeb3fc40d706ff3f60c85469cad663de2ad2372
sk <sk>
2013-07-16 00:16:38 +0800

    modify directory name and add 1link_8relay_8sec directory

--- 7.9_paper_line_pcture/8sector_new_thought/1link/7.6-1link-1.12relay/caldistance.m
+++
@@ -1,6 +0,0 @@
-function output=caldistance(locate_node,i_temp,j_temp)
-difx=locate_node(1,j_temp)-locate_node(1,i_temp);
-dify=locate_node(2,j_temp)-locate_node(2,i_temp);
- stadistance=sqrt(difx*difx+dify*dify);
- 
-output=stadistance;
